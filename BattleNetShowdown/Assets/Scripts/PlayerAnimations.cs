9ba0827919eb582af34aa7f03c005ad876d2ebc0
Jay <jasonkleitz@gmail.com>
2021-08-11 23:16:04 -0400

    Adds Swipe attack
    Now has a button for a wide swipe attache/

--- BattleNetShowdown/Assets/Scripts/PlayerAnimations.cs
+++ BattleNetShowdown/Assets/Scripts/PlayerAnimations.cs
@@ -11,22 +11,6 @@ public class PlayerAnimations : MonoBehaviour
     
     public Sprite idleSprite, shootingSprite, attackSprite;
 
-    void Slash()
-    {
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = attackSprite;
-        slashSFX.Play();
-
-        Debug.DrawRay(transform.position + new Vector3(1, -0.5f, 0), transform.TransformDirection(Vector2.right) * .45f, Color.red, .5f);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(1, -0.5f, 0), transform.TransformDirection(Vector2.right), .45f);
-
-        if(hit)
-        {
-            Debug.Log("Hit Something : " + hit.collider.name);
-            hit.transform.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-            hitSound.Play();
-        }   
-    }
-
   void Shoot()
     {
         this.gameObject.GetComponent<SpriteRenderer>().sprite = shootingSprite;
@@ -41,22 +25,59 @@ public class PlayerAnimations : MonoBehaviour
             hitSound.Play();
         }
     }
+    void Slash()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = attackSprite;
+        slashSFX.Play();
 
+        //TODO: clean up length and position
+        Debug.DrawRay(transform.position + new Vector3(1, -0.5f, 0), transform.TransformDirection(Vector2.right) * .45f, Color.red, .5f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(1, -0.5f, 0), transform.TransformDirection(Vector2.right), .45f);
 
+        if(hit)
+        {
+            Debug.Log("Hit Something : " + hit.collider.name);
+            hit.transform.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            hitSound.Play();
+        }   
+    }
 
-    // Update is called once per frame
+    void Swipe()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = attackSprite;
+        slashSFX.Play();
+
+        //TODO: clean up length and position
+        Debug.DrawRay(transform.position + new Vector3(1, -1.5f, 0), transform.TransformDirection(Vector2.up) * 1.95f, Color.red, .5f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(1, -1.5f, 0), transform.TransformDirection(Vector2.up), 1.95f);
+
+        if(hit)
+        {
+            Debug.Log("Hit Something : " + hit.collider.name);
+            hit.transform.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            hitSound.Play();
+        }
+    }
+
+    //Player controls
     void Update()
     {        
-        //Buster Attack
+        //Shoot Attack
         if(Input.GetKeyDown(KeyCode.N))
             Shoot();
         if(Input.GetKeyUp(KeyCode.N))
             this.gameObject.GetComponent<SpriteRenderer>().sprite = idleSprite;
 
-        //Chip Attack
+        //Slash Attack
         if(Input.GetKeyDown(KeyCode.M))
             Slash();
         if(Input.GetKeyUp(KeyCode.M))
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = idleSprite;  
+
+        //Swipe Attack
+        if(Input.GetKeyDown(KeyCode.B))
+            Swipe();
+        if(Input.GetKeyUp(KeyCode.B))
             this.gameObject.GetComponent<SpriteRenderer>().sprite = idleSprite;  
     }
 }
--- BattleNetShowdown/Assets/Scripts/PlayerMovement.cs
+++ BattleNetShowdown/Assets/Scripts/PlayerMovement.cs
@@ -15,41 +15,40 @@ public class PlayerMovement : MonoBehaviour
 
     public Sprite idleSprite, shootingSprite, attackSprite;
 
-    void Slash()
-    {
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = attackSprite;
-        slashSFX.Play();
-
-/*
-
-*/
-        Debug.DrawRay(transform.position + new Vector3Int(1, 0, 0), transform.TransformDirection(Vector2.right) * .45f, Color.red, .5f);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3Int(1, 0, 0), transform.TransformDirection(Vector2.right), .45f);
-
-        if(hit)
-        {
-            Debug.Log("Hit Something : " + hit.collider.name);
-            hit.transform.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-            hitSound.Play();
-        }
 
 
-    }
+// !! Moved to PlayerAnimations.cs !!
+    //Attacks one square in front of the character
+    // void Slash()
+    // {
+    //     this.gameObject.GetComponent<SpriteRenderer>().sprite = attackSprite;
+    //     slashSFX.Play();
 
-  void Shoot()
-    {
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = shootingSprite;
-        Debug.DrawRay(transform.position + new Vector3Int(1, 0, 0), transform.TransformDirection(Vector2.right) * shotDistance, Color.red, .5f);
-        shootSFX.Play();
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3Int(1, 0, 0), transform.TransformDirection(Vector2.right), shotDistance);
+    //     Debug.DrawRay(transform.position + new Vector3Int(1, 0, 0), transform.TransformDirection(Vector2.right) * .45f, Color.red, .5f);
+    //     RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3Int(1, 0, 0), transform.TransformDirection(Vector2.right), .45f);
 
-        if(hit)
-        {
-            Debug.Log("Hit Something : " + hit.collider.name);
-            hit.transform.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-            hitSound.Play();
-        }
-    }
+    //     if(hit)
+    //     {
+    //         Debug.Log("Hit Something : " + hit.collider.name);
+    //         hit.transform.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    //         hitSound.Play();
+    //     }
+    // }
+
+    //   void Shoot()
+    //     {
+    //         this.gameObject.GetComponent<SpriteRenderer>().sprite = shootingSprite;
+    //         Debug.DrawRay(transform.position + new Vector3Int(1, 0, 0), transform.TransformDirection(Vector2.right) * shotDistance, Color.red, .5f);
+    //         shootSFX.Play();
+    //         RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3Int(1, 0, 0), transform.TransformDirection(Vector2.right), shotDistance);
+
+    //         if(hit)
+    //         {
+    //             Debug.Log("Hit Something : " + hit.collider.name);
+    //             hit.transform.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    //             hitSound.Play();
+    //         }
+    //     }
 
     // Update is called once per frame
     void Update()
@@ -63,17 +62,18 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.D) && !isMoving)
             StartCoroutine(MovePlayer(Vector3.right));
 
-        //Buster Attack
-        if(Input.GetKeyDown(KeyCode.Space))
-            Shoot();
-        if(Input.GetKeyUp(KeyCode.Space))
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = idleSprite;
+// !! Moved to PlayerAnimations.cs !!
+        // //Buster Attack
+        // if(Input.GetKeyDown(KeyCode.Space))
+        //     Shoot();
+        // if(Input.GetKeyUp(KeyCode.Space))
+        //     this.gameObject.GetComponent<SpriteRenderer>().sprite = idleSprite;
 
-        //Chip Attack
-        if(Input.GetKeyDown(KeyCode.RightAlt))
-            Slash();
-        if(Input.GetKeyUp(KeyCode.RightAlt))
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = idleSprite;    
+        // //Chip Attack
+        // if(Input.GetKeyDown(KeyCode.RightAlt))
+        //     Slash();
+        // if(Input.GetKeyUp(KeyCode.RightAlt))
+        //     this.gameObject.GetComponent<SpriteRenderer>().sprite = idleSprite;    
     }
 
 
