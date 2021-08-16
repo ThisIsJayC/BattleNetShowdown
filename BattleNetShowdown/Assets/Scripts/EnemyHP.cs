4d0c373908c27c1d7c1f558ac31c18cb466e17a2
Jay <jasonkleitz@gmail.com>
2021-08-16 01:05:57 -0400

    Add Enemy HP
    This update is the first iteration of the enemy HP bar. HP bar is docked under the Enemy object sprite

---
+++ BattleNetShowdown/Assets/Scripts/EnemyHP.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnemyHP : MonoBehaviour
+{
+    public TextMesh enemyHPTextbox;
+
+    public int enemyHP, maxHP;
+    
+    public AudioSource enemyHitSFX;
+
+    public int damage = 200;// <------------------
+    public void TakeDamage(int damage)
+    {
+        //Calculate damage
+        enemyHP = enemyHP - damage;
+
+        Debug.Log(transform.parent.name + " took " + damage + " damage");
+
+        //Deletes the parent object when the HP reaches 0
+        if (enemyHP <= 0)
+        {
+            Debug.Log("Enemy was destroyed");
+            enemyHitSFX = GetComponent<AudioSource>();
+            enemyHitSFX.Play();
+            //Deletes the game object too fast. Doesn't even play the sound :[
+            //Destroy(transform.parent.gameObject);
+        }  
+    }
+
+    void Start() 
+    {
+        //This is an extremely dirty way to typecast enemyHP as a string
+        //enemyHPTextbox.text = "HP: " + enemyHP;
+        enemyHPTextbox.text = enemyHP.ToString();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+        TakeDamage(damage);
+        enemyHPTextbox.text = enemyHP.ToString();
+        }
+    }
+}
