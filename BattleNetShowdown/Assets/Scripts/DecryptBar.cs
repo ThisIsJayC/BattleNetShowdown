6027ec483d12f1880af3f50c7c9eb5ddefb63674
Jay C <jasonkleitz@gmail.com>
2021-08-18 13:43:27 -0400

    Updates DECRYPT Bar
    This updates the DECRYPT bar so that it fills up over time, chimes and pulses when full.

--- BattleNetShowdown/Assets/Scripts/DecryptBar.cs
+++ BattleNetShowdown/Assets/Scripts/DecryptBar.cs
@@ -8,10 +8,17 @@ public class DecryptBar : MonoBehaviour
     [SerializeField]
     public Slider decryptSlider;
 
+    [SerializeField]
+    public AudioSource fullDecryptBarChime;
+
+    private bool fullbar = false;
+    private float glow = 0f;
+    private int glowDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        decryptSlider.value = 0;
+        decryptSlider.value = 90; //TODO: set the back to 0
     }
 
     // Update is called once per frame
@@ -19,8 +26,47 @@ public class DecryptBar : MonoBehaviour
     {
         if(decryptSlider.value < 100)
         {
-            Debug.Log(decryptSlider.value);
+            //Debug.Log(decryptSlider.value);
             decryptSlider.value += .05f;
         }
+
+        if(decryptSlider.value == 100 && fullbar == false)
+        {
+            fullDecryptBarChime.Play();
+            fullbar = true;
+        }
+
+
+        //Option A: Color cycle
+        // if (decryptSlider.value == 100)
+        // {
+        //     if (glow >= 1f)
+        //     {
+        //         //Debug.Log("glow value is " + glow); //TODO: remove
+        //         glow = 0f;
+        //     }
+        //     //Change the color of the Decrypt bar
+        //     decryptSlider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = Random.ColorHSV(glow, glow, 1f, 1f, 1f, 1f);
+        //     glow +=.003f;
+        // }
+
+
+        //Option B: Single color pulse
+        if (decryptSlider.value == 100)
+        {
+            if (glow >= 1f)
+            {
+                //Debug.Log("glow value is " + glow); //TODO: remove
+                glowDirection = -1;
+            }
+            if (glow <= 0f)
+            {
+                glowDirection = 1;
+            }
+            //Change the color of the Decrypt bar
+            decryptSlider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = Random.ColorHSV(0.7f, 0.7f, glow, glow, 1f, 1f);
+            glow += (.003f * glowDirection);
+        }
+
     }
 }
