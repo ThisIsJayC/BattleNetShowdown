eac12178321b38e21dc450ce5f660285da75c480
Jay <jasonkleitz@gmail.com>
2021-08-22 08:11:52 -0400

    Fix enemy AI
    Now the enemy AI will only move ONCE.

--- BattleNetShowdown/Assets/Scripts/EnemyAI.cs
+++ BattleNetShowdown/Assets/Scripts/EnemyAI.cs
@@ -7,31 +7,32 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
 
     private Vector3 targetLocation = new Vector3(0.5f, -1.5f, 0);
+    static bool isMoving = false;
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position != targetLocation)
+        if(transform.position != targetLocation && isMoving == false)
         {
         //Wait 2 seconds and then move to the correct location
         StartCoroutine(MoveWait(2.0f, targetLocation));
 
-            IEnumerator MoveWait(float s, Vector3 targetLocation)
+        IEnumerator MoveWait(float s, Vector3 targetLocation)
             {
+                isMoving = true;
                 yield return new WaitForSeconds(s);
                 //TODO: Figure out why this doesn't end after 2 seconds.
-                //It repeats for 123 frames.
                 Debug.Log("Waited 2 seconds");
                 transform.position = targetLocation;
 
                 //Does not kick out of the Coroutine
                 StopCoroutine(MoveWait(2.0f, targetLocation));
-
+                isMoving = false;
             }
         }
 
