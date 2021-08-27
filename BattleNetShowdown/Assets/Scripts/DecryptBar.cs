ee82f1428b076d5c2ff51dd17264d26b06d45354
Jay <jasonkleitz@gmail.com>
2021-08-27 00:17:35 -0400

    Add functions to double/halve the DECRYPT bar speed, or fill instantly
    Adding functions to be used way in the future

--- BattleNetShowdown/Assets/Scripts/DecryptBar.cs
+++ BattleNetShowdown/Assets/Scripts/DecryptBar.cs
@@ -15,8 +15,9 @@ public class DecryptBar : MonoBehaviour
     private float glow = 0f;
     private int glowDirection;
 
-    //Set the length of each DECRYP round in seconds below:
-    public static float roundLength = 15f;
+    //Set the length of each DECRYPT round in seconds below:
+    public const float originalRoundLength = 15.0f;
+    public static float roundLength = originalRoundLength;
 
     private float decryptBarIncrement = 1 / (roundLength / 100f * 60f);
 
@@ -25,22 +26,24 @@ public class DecryptBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        decryptSlider.value = 0; //TODO: set the back to 0
+        decryptSlider.value = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        decryptBarIncrement = 1 / (roundLength / 100f * 60f);
+
         if(decryptSlider.value < 100)
         {
-            timer += Time.deltaTime;//TODO: Remove debugging code
-            //Debug.Log(decryptSlider.value);
+            timer += Time.deltaTime; //TODO: Remove debugging code
+            //Debug.Log(decryptSlider.value); //TODO: Remove debugging code
             decryptSlider.value += decryptBarIncrement;
         }
 
         if(decryptSlider.value == 100 && fullbar == false)
         {
-            Debug.Log("Time elapsed: " + timer);//TODO: Remove debugging code
+            Debug.Log("Time elapsed: " + timer); //TODO: Remove debugging code
             fullDecryptBarChime.Play();
             fullbar = true;
         }
@@ -57,7 +60,6 @@ public class DecryptBar : MonoBehaviour
         //     decryptSlider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = Random.ColorHSV(glow, glow, 1f, 1f, 1f, 1f);
         //     glow +=.003f;
         // }
-
 
         //Option B: Single color pulse
         if (decryptSlider.value == 100)
@@ -77,4 +79,30 @@ public class DecryptBar : MonoBehaviour
         }
 
     }
+
+        // 2X Faster
+        void DecyptBar2XSpeed()
+        {
+            {
+                if(roundLength != 2 * originalRoundLength)
+                    roundLength = roundLength * 2;
+            }
+        }
+
+        // 1/2 Slower
+        void DecryptBarHalfSpeed()
+        {
+            {
+                if(roundLength != originalRoundLength / 2)
+                    roundLength = roundLength / 2;
+            }
+        }
+
+        //Full bar
+        void DecryptBarFill()
+        {
+            {
+                decryptSlider.value = 100;
+            }
+        }
 }
