d5ef0f717524a5038692ad9697a782379025a92c
Jay <jasonkleitz@gmail.com>
2021-08-31 22:56:26 -0400

    Make slight changes to attack queue system
    Verified that  I can still change the sprites. TODO: Store attacks in array

--- BattleNetShowdown/Assets/Scripts/AttackQueue.cs
+++ BattleNetShowdown/Assets/Scripts/AttackQueue.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class AttackQueue : MonoBehaviour
 {
-    //public Sprite currentAttack;
+    //public Sprite currayOfSpritesentAttack;
 
     public Sprite lobAttackSprite, punchAttackSprite, slashAttackSprite;
+            int i = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        //FindObjectOfType<AttackScript>(Update().attackArray[0]);
+
+        Sprite[] arrayOfSprites = {lobAttackSprite, punchAttackSprite, slashAttackSprite};
+
+
+        //FindObjectOfType<AttackScript>(Update().attackArrayOfSpritesay[0]);
         this.gameObject.GetComponent<SpriteRenderer>().sprite = lobAttackSprite;
-        // Set the Attack Queue slot (orange square) to be the first item in the queue (attackArray[0])
-        // Once the forwards button is pressed, set the next attack sprite (attackArray[1])
+        // Set the Attack Queue slot (orange square) to be the first item in the queue (attackArrayOfSpritesay[0])
+        // Once the forwards button is pressed, set the next attack sprite (attackArrayOfSpritesay[1])
     }
 
     // Update is called once per frame
     void Update()
     {
-
+            if(Input.GetKeyDown(KeyCode.RightAlt))
+                {
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = pun;
+                    //Debug.Log(arrayOfSprites[i]);
+                    i++;
+                }
     }
 }
