602e82707a4230366c00a1810bb752706d5530de
Jay <jasonkleitz@gmail.com>
2021-09-03 00:28:56 -0400

    Prototype Attack Database
    This is a rough sketch of the Attack DB. This should store all of the information associated with the different attack types.

---
+++ BattleNetShowdown/Assets/Scripts/AttackDB.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackDB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    //Will need to make an array / structure that holds this information
+    /*
+    ATTACK DB PROTOTYPE
+
+    Unique ID number, Name of attack, Sprite Name, Damage, Function
+    1. Blast, BlastSprite, 30, Blast()
+    2. Guard, GuardSprite, 0, Guard()
+    3. Lob, LobSprite, 50, Lob()
+    4. Punch, PunchSprite, 80, Punch()
+    5. Slash, SlashSprite, 100, Slash()
+
+    */
+
+}
