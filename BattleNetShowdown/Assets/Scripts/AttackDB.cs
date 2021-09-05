72ba4609c4035cece0f150b88209947baeafb290
Jay <jasonkleitz@gmail.com>
2021-09-05 07:59:17 -0400

    Took notes / reformatted the Attack DB prototype

--- BattleNetShowdown/Assets/Scripts/AttackDB.cs
+++ BattleNetShowdown/Assets/Scripts/AttackDB.cs
@@ -16,17 +16,18 @@ public class AttackDB : MonoBehaviour
 
     }
 
+    //From Alwin: Basically, enums are like basic classes, you can save variables there and functions that change the value of stored variables. It is like a mini class specifically made to store variables you use often, so later on you can call the enum and write enum.Slash and it throws the value of Slash stored in the enum
 
     //Will need to make an array / structure that holds this information
     /*
     ATTACK DB PROTOTYPE
 
-    Unique ID number, Name of attack, Sprite Name, Damage, Function
-    1. Blast, BlastSprite, 30, Blast()
-    2. Guard, GuardSprite, 0, Guard()
-    3. Lob, LobSprite, 50, Lob()
-    4. Punch, PunchSprite, 80, Punch()
-    5. Slash, SlashSprite, 100, Slash()
+    Unique ID number    Name of attack      Sprite Name     Damage  Function
+    1                   Blast               BlastSprite     30      Blast()
+    2                   Guard               GuardSprite     0       Guard()
+    3                   Lob                 LobSprite       50      Lob()
+    4                   Punch               PunchSprite     80      Punch()
+    5                   Slash               SlashSprite     100     Slash()
 
     */
 
