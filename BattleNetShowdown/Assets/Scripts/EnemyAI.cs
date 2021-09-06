da58fd9bdc4c1fb658a1bd487f09339404a664bf
Jay <jasonkleitz@gmail.com>
2021-09-06 00:47:55 -0400

    Update random AI movement
    Enemy now moves around the battle grid to random spots

--- BattleNetShowdown/Assets/Scripts/EnemyAI.cs
+++ BattleNetShowdown/Assets/Scripts/EnemyAI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    //float leftMost = 0.5f, rightMost = 2.5f, topMost = -0.5f, bottomMost = -2.5f;
     private EnemyHP enemyHP;
     private Vector3 targetLocation = new Vector3(0.5f, -1.5f, 0);
     public bool isDead = true, isMoving = false;
@@ -22,19 +23,22 @@ public class EnemyAI : MonoBehaviour
             StopAllCoroutines(); // This stops the enemy AI from making a move after it's dead
         }
 
-        if(transform.position != targetLocation && isMoving == false && isDead == false)
+        // if(transform.position != targetLocation && isMoving == false && isDead == false)
+        if(isMoving == false && isDead == false)
         {
 
         //Wait 2 seconds and then move to the correct location
-        StartCoroutine(MoveWait(2.0f, targetLocation));
+        StartCoroutine(MoveWait(1.75f, targetLocation));
 
         IEnumerator MoveWait(float s, Vector3 targetLocation)
-            {
-                isMoving = true;
-                yield return new WaitForSeconds(s);
-                transform.position = targetLocation;
-                isMoving = false;
-            }
+        {
+            isMoving = true;
+            yield return new WaitForSeconds(s);
+            transform.position = targetLocation;
+            isMoving = false;
+        }
+
+        targetLocation = new Vector3(Random.Range(0, 3) + 0.5f, Random.Range(-2, 1) - 0.5f, 0);
         }
     }
 }
